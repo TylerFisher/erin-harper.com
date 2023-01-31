4867e6e894eaa7e7206dc3a24d28b0022c7847ed
Tyler Fisher <tyler@tylerjfisher.com>
2023-01-31 00:45:05 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["EB Garamond", "ui-serif", "Georgia", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
